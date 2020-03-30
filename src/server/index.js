39210e3d849519ffbe2b78e7e2e1a0ecb7e16bf4
moumen-soliman <moumensoliman@gmail.com>
2020-03-30 17:23:49 +0200

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,35 +1,52 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-var aylien = require("aylien_textapi");
+dotenv.config({path: './.env'})
+//Aylien API
+const AylienAPI = require('aylien_textapi');
 
-const app = express()
-dotenv.config();
-
-app.use(express.static('dist'))
-
-// set aylien API credentials
-// NOTICE that textapi is the name I used, but it is arbitrary.
-// You could call it aylienapi, nlp, or anything else, 
-//   just make sure to make that change universally!
-var textapi = new aylien({
+//set up Aylien API
+const textAPI = new AylienAPI({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
-});
-
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+//set aylien API credentials
+
+const app = express()
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json());
+app.use(express.static('dist'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('dist/index.html'))
+})
+
+app.post('/testing', async(req, res, next) => {
+    try {
+        var data = textAPI.sentiment({
+            'text': req.body.theText
+        }, function(error, response) {
+          console.log(response, error)
+            if (error === null) {
+                console.log(response);
+                res.send(response);
+            }
+        });
+    } catch(error) {
+        return next(error)
+    }
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
